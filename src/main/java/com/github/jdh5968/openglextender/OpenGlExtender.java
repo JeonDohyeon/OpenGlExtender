a3dfcf8d4b602a801be446168bf92249c8d9d742
jdh5968 <jdh59681@naver.com>
2022-05-19 23:36:06 +0900

    Debug mod subscribing method

--- src/main/java/com/github/jdh5968/openglextender/OpenGlExtender.java
+++ src/main/java/com/github/jdh5968/openglextender/OpenGlExtender.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("openGlExtender")
+@Mod("openglextender")
 public class OpenGlExtender {
 
     // Directly reference a log4j logger.
@@ -43,7 +43,7 @@ public class OpenGlExtender {
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("openGlExtender", "helloworld", () -> {
+        InterModComms.sendTo("openglextender", "helloworld", () -> {
             LOGGER.info("Revive Alpha Test to Forge 1.18");
             return "Hello world";
         });
